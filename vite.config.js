be5ad210f1846d7791a6979ef74211b2cdca7af1
Fazeel Rizvi <rizvifazeel@github.com>
2026-01-15 13:04:47 +0530

    Revamped the entire UI. - Added a dark mode theme with toggle functionality. - Redesigned components for a modern look and improved user experience. - Improved caching logic in React Query.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    mainFields: [],
+    alias: {
+      stream: 'stream-browserify',
+      util: 'util/'
+    }
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  }
 })
